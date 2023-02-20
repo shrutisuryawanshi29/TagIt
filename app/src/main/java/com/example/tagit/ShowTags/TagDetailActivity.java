83db8965983f41993326c7fa725c7af14252f26a
ShrutiSuryawanshi <shrutisuryawanshi@Admins-MacBook-Air.local>
2023-02-20 20:21:04 +0530

    added 10 colors to update tag

--- app/src/main/java/com/example/tagit/ShowTags/TagDetailActivity.java
+++ app/src/main/java/com/example/tagit/ShowTags/TagDetailActivity.java
@@ -35,13 +35,16 @@ public class TagDetailActivity extends AppCompatActivity implements View.OnClick
     EditText addDescriptionET;
     Button tagColor1Btn, tagColor2Btn, tagColor3Btn, tagColor4Btn, tagColor5Btn;
     View view_selected_1, view_selected_2, view_selected_3, view_selected_4, view_selected_5;
+
+    Button tagColor6Btn, tagColor7Btn, tagColor8Btn, tagColor9Btn, tagColor10Btn;
+    View view_selected_6, view_selected_7, view_selected_8, view_selected_9, view_selected_10;
     Button discardChangesBtn, updateTagBtn;
     RecyclerView tag_details_recyclerView;
     DBHandler dbHandler;
     ArrayList<TagEventsModel> tagEventsModelArrayList = new ArrayList<>();
 
-    boolean[] isColorSelectedArray = new boolean[]{false, false, false, false, false};
-    String[] colorArray = new String[]{"78AD92", "A0C3D2", "78ADAC", "7893AD", "9278AD"};
+    boolean[] isColorSelectedArray = new boolean[]{false, false, false, false, false, false, false, false, false, false};
+    String[] colorArray = new String[]{"78AD92", "A0C3D2", "78ADAC", "7893AD", "9278AD", "C3B091", "EAC7C7", "DBC970", "E6BA95", "FFAB91"};
 
     String selectedTagName;
 
@@ -67,6 +70,11 @@ public class TagDetailActivity extends AppCompatActivity implements View.OnClick
         tagColor3Btn = findViewById(R.id.btn_tag_color3);
         tagColor4Btn = findViewById(R.id.btn_tag_color4);
         tagColor5Btn = findViewById(R.id.btn_tag_color5);
+        tagColor6Btn = findViewById(R.id.btn_tag_color6);
+        tagColor7Btn = findViewById(R.id.btn_tag_color7);
+        tagColor8Btn = findViewById(R.id.btn_tag_color8);
+        tagColor9Btn = findViewById(R.id.btn_tag_color9);
+        tagColor10Btn = findViewById(R.id.btn_tag_color10);
         discardChangesBtn = findViewById(R.id.btn_discard_changes);
         updateTagBtn = findViewById(R.id.btn_update_tag);
         tag_details_recyclerView = findViewById(R.id.tag_details_recyclerView);
@@ -75,12 +83,22 @@ public class TagDetailActivity extends AppCompatActivity implements View.OnClick
         view_selected_3 = findViewById(R.id.view_selected_3);
         view_selected_4 = findViewById(R.id.view_selected_4);
         view_selected_5 = findViewById(R.id.view_selected_5);
+        view_selected_6 = findViewById(R.id.view_selected_6);
+        view_selected_7 = findViewById(R.id.view_selected_7);
+        view_selected_8 = findViewById(R.id.view_selected_8);
+        view_selected_9 = findViewById(R.id.view_selected_9);
+        view_selected_10 = findViewById(R.id.view_selected_10);
 
         tagColor1Btn.setOnClickListener(this);
         tagColor2Btn.setOnClickListener(this);
         tagColor3Btn.setOnClickListener(this);
         tagColor4Btn.setOnClickListener(this);
         tagColor5Btn.setOnClickListener(this);
+        tagColor6Btn.setOnClickListener(this);
+        tagColor7Btn.setOnClickListener(this);
+        tagColor8Btn.setOnClickListener(this);
+        tagColor9Btn.setOnClickListener(this);
+        tagColor10Btn.setOnClickListener(this);
         discardChangesBtn.setOnClickListener(this);
         updateTagBtn.setOnClickListener(this);
 
@@ -162,6 +180,26 @@ public class TagDetailActivity extends AppCompatActivity implements View.OnClick
                 setValuesInSelectedArray(4);
                 break;
 
+            case R.id.btn_tag_color6:
+                setValuesInSelectedArray(5);
+                break;
+
+            case R.id.btn_tag_color7:
+                setValuesInSelectedArray(6);
+                break;
+
+            case R.id.btn_tag_color8:
+                setValuesInSelectedArray(7);
+                break;
+
+            case R.id.btn_tag_color9:
+                setValuesInSelectedArray(8);
+                break;
+
+            case R.id.btn_tag_color10:
+                setValuesInSelectedArray(9);
+                break;
+
             case R.id.btn_discard_changes:
                 setDataForViews();
                 break;
@@ -173,7 +211,7 @@ public class TagDetailActivity extends AppCompatActivity implements View.OnClick
     }
 
     void setValuesInSelectedArray(int position) {
-        isColorSelectedArray = new boolean[]{false, false, false, false, false};
+        isColorSelectedArray = new boolean[]{false, false, false, false, false, false, false, false, false, false};
         isColorSelectedArray[position] = true;
 
         view_selected_1.setVisibility((position + 1) == 1 ? View.VISIBLE : View.INVISIBLE);
@@ -181,6 +219,11 @@ public class TagDetailActivity extends AppCompatActivity implements View.OnClick
         view_selected_3.setVisibility((position + 1) == 3 ? View.VISIBLE : View.INVISIBLE);
         view_selected_4.setVisibility((position + 1) == 4 ? View.VISIBLE : View.INVISIBLE);
         view_selected_5.setVisibility((position + 1) == 5 ? View.VISIBLE : View.INVISIBLE);
+        view_selected_6.setVisibility((position + 1) == 6 ? View.VISIBLE : View.INVISIBLE);
+        view_selected_7.setVisibility((position + 1) == 7 ? View.VISIBLE : View.INVISIBLE);
+        view_selected_8.setVisibility((position + 1) == 8 ? View.VISIBLE : View.INVISIBLE);
+        view_selected_9.setVisibility((position + 1) == 9 ? View.VISIBLE : View.INVISIBLE);
+        view_selected_10.setVisibility((position + 1) == 10 ? View.VISIBLE : View.INVISIBLE);
     }
 
     int gtIndexOfColor() {
